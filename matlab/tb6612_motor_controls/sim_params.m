f665a16a0db8fd6e055e01584afa5e2a452815c4
Devin Jaenicke <djaenicke.92@gmail.com>
2020-04-04 15:55:40 -0400

    Updated for new controller tuning

--- matlab/tb6612_motor_controls/sim_params.m
+++ matlab/tb6612_motor_controls/sim_params.m
@@ -1,12 +1,12 @@
 Ts = 0.025;         % Sample time
 
-L_Kp = 4.5;         % Proportional gain
-L_Ki = 10.0;        % Integral gain
-L_Kd = 0.3;         % Derivative gain
+L_Kp = 1.95;        % Proportional gain
+L_Ki = 3.90;        % Integral gain
+L_Kd = 0.05;        % Derivative gain
 
-R_Kp =  4.5;        % Proportional gain
-R_Ki =  12.0;       % Integral gain
-R_Kd =  0.3;        % Derivative gain
+R_Kp =  1.45;       % Proportional gain
+R_Ki =  3.50;       % Integral gain
+R_Kd =  0.05;       % Derivative gain
 
 L_Ke = 0.215;       % Voltage to speed constants
 R_Ke = 0.255;
@@ -15,5 +15,5 @@ Min_V = 0.0;        % Minimum voltage for the motors to spin
 Driver_Vdrop = 0.1; % Voltage drop across motor driver
 Max_V = 6.0 + Driver_Vdrop;
 
-alpha = 0.4;
+alpha = 0.2;
 
