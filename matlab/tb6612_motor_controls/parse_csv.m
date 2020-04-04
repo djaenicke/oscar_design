2222baf1f43779c7f5092841537846b298f0173e
Devin Jaenicke <djaenicke.92@gmail.com>
2020-04-04 15:50:27 -0400

    Update parse_csv.m

--- matlab/tb6612_motor_controls/parse_csv.m
+++ matlab/tb6612_motor_controls/parse_csv.m
@@ -4,3 +4,11 @@ speed_sp = data(:,2);
 r_wheel_speed = data(:,3);
 l_wheel_speed = data(:,4);
 v_batt = data(:,5);
+r_wheel_dc = data(:,6);
+l_wheel_dc = data(:,7);
+
+figure(1);
+plot(l_wheel_speed);
+hold on;
+plot(r_wheel_speed);
+legend('left', 'right');
